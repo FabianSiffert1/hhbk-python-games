67f2ac139c4c47845886d1cbbd7d8800f5c82ffa
OgSherlockCode <abbokanal3@gmail.com>
2022-04-20 17:43:56 +0200

    Player Are Clickable

--- Frames/gameScreen.py
+++ Frames/gameScreen.py
@@ -4,8 +4,8 @@ from tkinter import ttk
 class GameScreen:
     """Game Screen Content"""
 
-    global vFrame
     canvas = ""
+    frame = ""
 
     cellSize = 50
     cellRows = 6
@@ -23,17 +23,13 @@ class GameScreen:
         ]
 
     def __init__(self,vFrame):
-        frame = ttk.Frame(vFrame.mainWindow, padding=10)
+        self.frame = ttk.Frame(vFrame.mainWindow, padding=10)
 
-        button = Button(frame, text="Exit", command= lambda: vFrame.openScreen("start")).grid(column=0,row=0,sticky=W,padx=0,pady=0)
+        Button(self.frame, text="Exit", command= lambda: vFrame.openScreen("start")).grid(column=0,row=0,sticky=W,padx=0,pady=0)
 
-
-        self.drawBoard(frame)
-
-        self.drawPlayer(frame)
+        self.updateCanvas()
         
-
-        vFrame.mainWindow.frm = frame
+        vFrame.mainWindow.frm = self.frame
         vFrame.mainWindow.frm.grid()
 
     def drawPlayer(self,frame):
@@ -43,13 +39,21 @@ class GameScreen:
             while x < self.cellRows:
                 xs = x * self.cellSize
                 ys = y * self.cellSize
-                if self.board[y][x] is not 0:
-                    self.canvas.create_oval(xs, ys, xs + self.cellSize, ys + self.cellSize, fill=self.player_colors[self.board[y][x] - 1],width=0)
-                
+                if self.board[y][x] != 0:
+                    self.canvas.create_oval(xs, ys, xs + self.cellSize, ys + self.cellSize, fill=self.player_colors[self.board[y][x] - 1],width=0, tags="playerbutton")
+                    self.canvas.tag_bind("playerbutton","<Button-1>", self.playerClicked)
+
                 x = x + 1
             x = 0
             y = y + 1
 
+    def playerClicked(self):
+        self.board[3][3] = 2
+        self.updateCanvas()
+        
+    def updateCanvas(self):
+        self.drawBoard(self.frame)
+        self.drawPlayer(self.frame)
 
     def drawBoard(self,frame):
         self.canvas = Canvas(frame, bg="white", height=self.cellSize*self.cellRows, width=self.cellSize*self.cellRows)
@@ -65,13 +69,13 @@ class GameScreen:
                 self.canvas.create_rectangle(xs, ys, xs + self.cellSize, ys + self.cellSize, fill=self.colors[colorIndex])
 
                 colorIndex = colorIndex + 1
-                if colorIndex is 2:
+                if colorIndex == 2:
                     colorIndex = 0
                  
                 x = x + 1
 
             colorIndex = colorIndex + 1
-            if colorIndex is 2:
+            if colorIndex == 2:
                 colorIndex = 0
 
             x = 0
