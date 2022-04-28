e87281705f5e44cffdc60099bc9b136189850dc2
User <abbokanal3@gmail.com>
2022-04-28 12:38:39 +0200

    Ai is Working but stupid

--- Game/gameScore.py
+++ Game/gameScore.py
@@ -10,20 +10,9 @@ class GameScore:
         ]
 
     def evaluateScore(self, figurePositions , team, cellCount):
-
-
-        x = 0
-        y = 0
-        figureCount = 0
         scoreReturn = 0
         result = [0,0,0,0,0,0]
-        while x < cellCount:
-            while y < cellCount:
-                if figurePositions[y][x] == team:
-                    figureCount += 1
-                y +=1
-            y = 0
-            x+=1
+
         
         x = 0
         y = 0
@@ -32,10 +21,10 @@ class GameScore:
                 if figurePositions[y][x] == team:
                     if team == 1:
                         result[x] = cellCount - y - 1
-                        scoreReturn += result[x] * result[x] / figureCount
+                        scoreReturn += result[x] * result[x] / cellCount
                     else:
                         result[x] = y 
-                        scoreReturn += result[x] * result[x] / figureCount
+                        scoreReturn += result[x] * result[x] / cellCount
                 y +=1
             y = 0
             x+=1
